6bb0a586998087900302642ce85ac59ef0525c37
oliverkardos <kardos.oliver@gmail.com>
2021-03-03 14:29:15 +0100

    Testing the pipe (VS Git stack)

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace iskolacsengo
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. v1
         /// </summary>
         [STAThread]
         static void Main()
