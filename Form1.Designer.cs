4ad400dbeaf6d84d75aff1775630de35523fe78b
oliverkardos <kardos.oliver@gmail.com>
2021-05-13 16:00:06 +0200

    add sound playback, remove broken features

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,22 +30,27 @@ namespace iskolacsengo
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.actualTime = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // actualTime
             // 
             this.actualTime.AutoSize = true;
-            this.actualTime.Font = new System.Drawing.Font("Quesat Demo", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actualTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.actualTime.ForeColor = System.Drawing.Color.White;
             this.actualTime.Location = new System.Drawing.Point(403, 8);
             this.actualTime.Name = "actualTime";
-            this.actualTime.Size = new System.Drawing.Size(0, 43);
+            this.actualTime.Size = new System.Drawing.Size(0, 39);
             this.actualTime.TabIndex = 0;
             this.actualTime.Click += new System.EventHandler(this.actualTime_Click);
             // 
@@ -58,7 +63,7 @@ namespace iskolacsengo
             // monthCalendar1
             // 
             this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
-            this.monthCalendar1.Location = new System.Drawing.Point(22, 28);
+            this.monthCalendar1.Location = new System.Drawing.Point(2, 59);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 1;
             // 
@@ -67,19 +72,20 @@ namespace iskolacsengo
             this.button1.BackColor = System.Drawing.Color.YellowGreen;
             this.button1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(22, 219);
+            this.button1.Location = new System.Drawing.Point(2, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 59);
             this.button1.TabIndex = 2;
             this.button1.Text = "Normál csengetés";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.YellowGreen;
             this.button2.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(142, 219);
+            this.button2.Location = new System.Drawing.Point(122, 247);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 59);
             this.button2.TabIndex = 3;
@@ -91,12 +97,38 @@ namespace iskolacsengo
             this.button3.BackColor = System.Drawing.Color.YellowGreen;
             this.button3.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(268, 219);
+            this.button3.Location = new System.Drawing.Point(242, 247);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(114, 59);
             this.button3.TabIndex = 4;
             this.button3.Text = "Egyéni csengetés";
             this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.YellowGreen;
+            this.button4.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(362, 247);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 59);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Próba csengetés";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(315, 1);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(214, 46);
+            this.axWindowsMediaPlayer1.TabIndex = 6;
             // 
             // Form1
             // 
@@ -104,6 +136,8 @@ namespace iskolacsengo
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(531, 306);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -112,6 +146,7 @@ namespace iskolacsengo
             this.Name = "Form1";
             this.Text = "Iskolacsengő Applikáció";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,8 +160,11 @@ namespace iskolacsengo
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-    //    private System.Windows.Forms.Label label1;
-     //   private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        //    private System.Windows.Forms.Label label1;
+        //   private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,11 @@ namespace iskolacsengo
 {
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void actualTime_Click(object sender, EventArgs e)
@@ -29,6 +31,19 @@ namespace iskolacsengo
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(mp3,wav,mp4,mov,wmv,mpg)|*.mp3;*.wav;*.mp4;*.mov;*.wmv;*.mpg|all files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
 
         }
     }
